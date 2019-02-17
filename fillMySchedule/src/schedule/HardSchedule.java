9e51e8f15570dbb7befa797c7a9ea21933cbb155
Cheng Loong <chengloongkong@hotmail.com>
2019-02-16 19:50:50 -0800

    Building the calendar function
    started

--- fillMySchedule/src/schedule/HardSchedule.java
+++ fillMySchedule/src/schedule/HardSchedule.java
@@ -8,7 +8,7 @@ public class HardSchedule {
 	/*********************************
 	 * Constructor: HardSchedule()
 	 * Initializes an array of events with the given max size and sets the entry number to 0
-	 */
+	 *********************************/
 	public HardSchedule(int capacity) throws IllegalArgumentException{
 		if (capacity <= 0) {
 			throw new IllegalArgumentException("Capacity must be greater than 0.");
@@ -20,7 +20,38 @@ public class HardSchedule {
 	/*********************************
 	 * Constructor: HardSchedule()
 	 * Initializes an array of events with the given max size and sets the entry number to 0
-	 */
+	 *********************************/
+	public HardSchedule() {
+		this.HardSchedule(DEFAULT_SIZE);
+	}
+	
+	/*********************************
+	 * Method: add()
+	 * adds an event to the calendar and increments size by 1
+	 * increases the size of the database if it is full
+	 *********************************/
+	public void add(Event event) {
+		calendar[size] = event;
+		size++;
+		if (size >= calendar.length) {
+			expand();
+		}
+	}
+	
+	/*********************************
+	 * Method: expand()
+	 * creates a new database of double the size of the old one
+	 * copies the old entries into the new database
+	 * returns the new database
+	 *********************************/
+	public void expand() {
+		int newlength = size*2;
+		Event[] newCalendar = new Event [newlength];
+		for (int i = 0; i < size; i++) {
+			newCalendar[i] = calendar[i];
+		}
+		calendar = newCalendar;
+	}
 	
 	
 	public static void main(String[] args) {
